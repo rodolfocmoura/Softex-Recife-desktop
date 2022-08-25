cf599aa1ffd3b71cd3593f25828614708c402d4d
rodolfocmoura <rodolfo.carvalho.moura@gmail.com>
2022-08-25 11:28:17 -0300

    Create packagecrossfit.java

---
+++ Logica de Programação/packagecrossfit.java
@@ -0,0 +1,54 @@
+package br.com.crossfit.data;
+    public class Cadastro {
+
+        public constructor (data, nome) {
+            this.data = data;
+            this.nome = nome;
+        }
+        set carga(value) {    
+            this.data[ carga ]= value;
+        }
+        get carga() {   
+            return this.data;
+        }
+        set name(value) { 
+            this.nome[ name ]= string;
+        }
+        get name(){
+            return this.nome;
+        }
+
+        }
+        function escreveinfo(){
+        console.log("Temos o atleta "+this.nome+" com a carga "+this.data".")
+        }
+        
+    }
+
+package br.com.biblioteca.cadastro;
+    public class Cadastro {
+
+        public constructor (data, nome) {
+            this.data = adesao;
+            this.nome = livro;
+        }
+        set carga(value) {    
+            this.data[ adesao ]= value;
+        }
+        get carga() {   
+            return this.data;
+        }
+        set name(value) { 
+            this.nome[ livro ]= string;
+        }
+        get name(){
+            return this.nome;
+        }
+
+        }
+        function escreveinfo(){
+        console.log("O livro "+this.nome+" foi adicionado no dia "+this.data".")
+        }
+        
+    }
+
